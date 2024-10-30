f8673d611aede832d00f81fb9b2fbc4104653e04
Supre_le_sucre <wilhemblondel@gmail.com>
2024-10-30 20:45:32 +0100

    Added conform.nvim for better formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+	'stevearc/conform.nvim',
+	ft = { "lua, python", "c" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			c = { "clang_format" }
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+}
